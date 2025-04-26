e8d784fbf4af5131c3dd0e7d15a8d1d89646ce67
FrostyFox33 <43838681+FrostyFox33@users.noreply.github.com>
2025-04-26 15:41:02 +0300

    .

--- mechabellum_builder.py
+++ mechabellum_builder.py
@@ -375,16 +375,23 @@ def run_app():
 
             chaf_score = 0
             arc_score = 0
+
             # if name u is chaff unit and round==1 add to score
             if u in chaf_units and round_num == 1:
                 if enemy_has_counter(u):
-                    chaf_score = 7
+                    chaf_score = 8
                 chaf_score = 10
             # if build doesn't have arclight and round==1 add to score
             if "Arclight" not in my_units and round_num == 1:
-                arc_score = 7
+                arc_score = 8
                 if any(chaf in enemy_units for chaf in chaf_units):
                     arc_score = 11
+
+            # if chaf unit is in my build and round!=1 add to score
+            if u in chaf_units and round_num != 1:
+                chaf_score = (
+                    -3
+                )  # we already have the tip in place so usually we don't want to upgrade it
 
             # coverage
             already = {
